9acb9e2371313e5fadd87c7ce68bdb4a855598ea
MalondaClement <clementmalonda@yahoo.fr>
2019-06-05 21:51:11 +0200

    Mise en place des éléments du modèle. Ajout des fichiers: Question.swift (structure Question), Game.swift (class Game qui gère tout le fonctionnement d'une partie) et QuestionManager.swift (qui gère le téléchargement des questions)

---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Clément Malonda on 05/06/2019.
+//  Copyright © 2019 Clément Malonda. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+	var title = ""
+	var isCorrect = false
+}
