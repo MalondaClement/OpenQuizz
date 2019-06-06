d214b56235f1f2ccd7c1699f9d999994e4d58aa3
MalondaClement <clementmalonda@yahoo.fr>
2019-06-06 11:47:51 +0200

    Mise ne place de la vue avant connection

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Clément Malonda on 06/06/2019.
+//  Copyright © 2019 Clément Malonda. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+	
+	@IBOutlet private var label: UILabel?
+	@IBOutlet private var icon: UIImageView?
+}
