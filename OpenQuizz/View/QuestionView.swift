97eb7a6749fe80309fd6f888f7fe74bdec1de2c7
MalondaClement <clementmalonda@yahoo.fr>
2019-06-16 22:03:15 +0200

    AJout du fichier QestionView et de la classe qui porte le même nom et qui gère l'affichage de la question.

--- OpenQuizz/View/QuestionView.swift
+++ OpenQuizz/View/QuestionView.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class QuestionView: UIView {
 	
-	@IBOutlet private var label: UILabel?
-	@IBOutlet private var icon: UIImageView?
+	@IBOutlet private var label: UILabel!
+	@IBOutlet private var icon: UIImageView!
+	
+	enum Style {
+		case correct, incorrect, standard
+	}
+	var style: Style = .standard{
+		didSet{
+			setStyle(style)
+		}
+	}
+	
+	private func setStyle(_ style: Style){
+		switch style {
+		case .correct:
+			backgroundColor = #colorLiteral(red: 0.7842472196, green: 0.9260502458, blue: 0.6268314719, alpha: 1)
+			icon.image = UIImage(named: "Icon Correct")
+			icon.isHidden = false
+		case .incorrect:
+			backgroundColor = #colorLiteral(red: 0.9313799739, green: 0.5266035199, blue: 0.5792709589, alpha: 1)
+			icon.image = UIImage(named: "Icon Error")
+			icon.isHidden = false
+		case .standard:
+			backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+			icon.isHidden = true
+			
+		}
+	}
+	
+	var title = "" {
+		didSet {
+			label.text = title
+		}
+	}
 }
