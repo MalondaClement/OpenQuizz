6e2f2a66facf4342b6da3fa2b4c6c329f2ecbe2b
MalondaClement <clementmalonda@yahoo.fr>
2019-06-19 11:41:47 +0200

    Finalisation de l'application avec l'ajout d'animation

--- OpenQuizz/Controller/ViewController.swift
+++ OpenQuizz/Controller/ViewController.swift
@@ -96,7 +96,24 @@ class ViewController: UIViewController {
 		}
 		scoreLabel.text = "\(game.score) / 10"
 		
+		let screenWidth = UIScreen.main.bounds.width
+		var translationTransform: CGAffineTransform
+		if questionView.style == .correct{
+			translationTransform = CGAffineTransform(translationX: screenWidth, y: 0)
+		}else{
+			translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
+		}
+		UIView.animate(withDuration: 0.3, animations: {
+			self.questionView.transform = translationTransform
+		}) { (success) in
+			if success {
+				self.showQuestionView()
+			}
+		}
+	}
+	private func showQuestionView(){
 		questionView.transform = .identity
+		questionView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
 		questionView.style = .standard
 		
 		switch game.state {
@@ -105,6 +122,9 @@ class ViewController: UIViewController {
 		case .over:
 			questionView.title = "Game Over"
 		}
+		UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+			self.questionView.transform = .identity
+		}, completion: nil)
 	}
 }
 
